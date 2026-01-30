5a287f080ffbb0a258e1f5a391659a217c5ecbb8
Timofey2339 <kirpatimofey@gmail.com>
2026-01-30 02:31:17 +0200

    Implement Rage A-60 entity with sound and effects
    Added functionality for Rage A-60 entity with sound effects and atmosphere changes.

---
+++ A-60 Raw.lua
@@ -0,0 +1,63 @@
+local sound = Instance.new("Sound")
+sound.Parent = workspace
+sound.SoundId = "rbxassetid://933230732"
+sound.Volume = 5
+sound.Looped = false
+sound.PlaybackSpeed = 0.75
+
+local pitch = Instance.new("PitchShiftSoundEffect")
+pitch.Parent = sound
+pitch.Octave = 0.935
+
+sound:Play()
+
+wait(23)
+local spawner = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Entity%20Spawner/V2/Source.lua"))()	
+local entity = spawner.Create({
+        Entity = {
+            Name = "Rage A-60",
+            Asset = "rbxassetid://132439862574228",
+            HeightOffset = 0
+        },
+        Lights = {
+            Flicker = { Enabled = true, Duration = 6},
+            Shatter = true, Repair = false
+        },
+        Earthquake = { Enabled = true },
+        CameraShake = { Enabled = true, Range = 100, Values = {20, 20, 0.1, 1} },
+        Movement = { Speed = 300, Delay = 2, Reversed = false },
+        Rebounding = { Enabled = false, Type = "Ambush", Min = 1, Max = 1, Delay = 1.0 },
+        Damage = { Enabled = true, Range = 40, Amount = 125 },
+        Crucifixion = { Enabled = true, Range = 40, Resist = false, Break = true },
+        Death = {
+            Type = "Guiding",
+            Hints = {"You died by Rage A-60", "if you hear scream hide", "now","Bye Bye"}, 
+            Cause = "Rage A-60"
+        }
+    })
+
+    entity:SetCallback("OnSpawned", function()
+wait(5)
+        SetAtmosphere(Color3.fromRGB(100, 150, 255), 0.5)
+        local model = game.Workspace:FindFirstChild("Rage A-60")
+        if model then
+            for _, v in pairs(model:GetDescendants()) do
+                if v:IsA("Sound") then
+                    v.SoundId = "rbxassetid://111930358743197"
+                    v.Volume = 2
+                    v.PlaybackSpeed = 1
+                elseif v:IsA("BasePart") then
+                    v.Color = Color3.fromRGB(0, 0, 80)
+                    v.Material = Enum.Material.Neon
+                    local light = Instance.new("PointLight", v)
+                    light.Color = Color3.fromRGB(0, 255, 255)
+                    light.Range = 60; light.Brightness = 6; light.Shadows = true
+                elseif v:IsA("Decal") then
+                    v.Texture = "rbxassetid://11118765532"
+                end
+            end
+        end
+    end)
+
+    entity:SetCallback("OnDespawned", function() ClearAtmosphere() end)
+    entity:Run()
