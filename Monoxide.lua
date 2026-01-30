6f40724d53acbc20f278d6b594a4ec0870f32845
Timofey2339 <kirpatimofey@gmail.com>
2026-01-30 13:05:38 +0200

    Implement sound and GUI for Monoxide entity
    Added sound playback and GUI creation for Monoxide entity.

--- Monoxide.lua
+++ Monoxide.lua
@@ -1,3 +1,30 @@
+local sound = Instance.new("Sound")
+sound.Parent = workspace
+sound.SoundId = "rbxassetid://166047422"
+sound.Volume = 7
+sound.Looped = false
+sound.PlaybackSpeed = 0.8
+
+local pitch = Instance.new("PitchShiftSoundEffect")
+pitch.Parent = sound
+pitch.Octave = 1.25
+
+sound:Play()
+
+local gui = Instance.new("ScreenGui")
+gui.Parent = game.Players.LocalPlayer.PlayerGui
+gui.IgnoreGuiInset = true
+
+local frame = Instance.new("Frame")
+frame.Parent = gui
+frame.BackgroundColor3 = Color3.fromRGB(0, 90, 175)
+frame.Position = UDim2.new(0.5, 0, 0.5, 0)
+frame.AnchorPoint = Vector2.new(0.5,0.5)
+frame.Size = UDim2.new(3, 0, 3, 0)
+frame.BackgroundTransparency = 0.5
+game:GetService("TweenService"):Create(frame,TweenInfo.new(1.2,Enum.EasingStyle.Sine),{BackgroundTransparency=1}):Play()
+wait(1.3)
+frame:Destroy()
 local spawner = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Entity%20Spawner/V2/Source.lua"))()	
 local entity = spawner.Create({
         Entity = {
