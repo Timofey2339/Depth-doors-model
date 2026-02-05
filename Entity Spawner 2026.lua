e572ad673420053bd160fde3292ec51aae09bd38
Timofey2339 <kirpatimofey@gmail.com>
2026-02-05 12:47:28 +0200

    Refactor tab and section creation methods

--- Entity Spawner 2026.lua
+++ Entity Spawner 2026.lua
@@ -37,19 +37,20 @@ local Window = Rayfield:CreateWindow({
       Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
    }
 })
+local Tab = Window:CreateTab("idk", 4483362458) -- Title, Image
 
 -- MAIN SPAWNING
-local Main = Window:NewTab("Custom Entity Spawner")
-local MainSection = Main:NewSection("Custom")
-local Main2 = Window:NewTab("Entity Spawner")
-local Main2Section = Main2:NewSection("Basic")
-local Main3 = Window:NewTab("Items")
-local Main3Section = Main3:NewSection("Giver")
-local Main4 = Window:NewTab("Modes Entities Spawner")
-local Main4Section = Main4:NewSection("Hardcore")
-local Main5Section = Main4:NewSection("Impossible")
+local Main = Window:CreateTab("Custom Entity Spawner")
+local MainSection = Main:CreateSection("Custom")
+local Main2 = Window:CreateTab("Entity Spawner")
+local Main2Section = Main2:CreateSection("Basic")
+local Main3 = Window:CreateTab("Items")
+local Main3Section = Main3:CreateSection("Giver")
+local Main4 = Window:CreateTab("Modes Entities Spawner")
+local Main4Section = Main4:CreateSection("Hardcore")
+local Main5Section = Main4:CreateSection("Impossible")
 
-MainSection:NewButton("Greg", "Spawning", function()
+MainSection:CreateButton("Greg", "Spawning", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Timofey2339/Depth-doors-model/refs/heads/main/Greg%20Raw.lua"))()
   end)
 MainSection:NewButton("A-25 From Rooms but Doors", "Spawning", function()
