99db2fb7a6e071816f7d41cf529e46ae95b54b9d
Timofey2339 <kirpatimofey@gmail.com>
2026-03-11 13:13:08 +0200

    Enhance jumpscare GUI with dynamic effects
    Added SizeValue for dynamic Face size adjustment and modified Background colors during jumpscare. Updated sound effects properties and their parent instances.

--- Greg Raw.lua
+++ Greg Raw.lua
@@ -64,6 +64,9 @@ entity:SetCallback("OnDamagePlayer", function(newHealth)
     local JumpscareGui = Instance.new("ScreenGui")
     local Background = Instance.new("Frame")
     local Face = Instance.new("ImageLabel")
+	local SizeValue = Instance.new("NumberValue")
+
+	SizeValue.Value = -0.15
 
     JumpscareGui.Name = "Jumpscare"
     JumpscareGui.IgnoreGuiInset = true
@@ -81,26 +84,55 @@ entity:SetCallback("OnDamagePlayer", function(newHealth)
     Face.BackgroundTransparency = 1
     Face.Position = UDim2.new(0.5, 0, 0.5, 0)
     Face.ResampleMode = Enum.ResamplerMode.Pixelated
-    Face.Size = UDim2.new(0, 150, 0, 150)
+    Face.Size = UDim2.new(0.25, 0, 0.3, 0)
     Face.Image = "rbxassetid://10914800940"
 
     Background.Parent = JumpscareGui
     Face.Parent = Background
+	SizeValue.Parent = Background
 local scare = Instance.new("Sound")
-scare.Parent = JumpscareGui
+scare.Parent = Background
 scare.Name = "Jumpscare"
 scare.SoundId = "rbxassetid://135750666095391"
 scare.PlaybackSpeed = 2
 scare.Volume = 3
 
 local shift = Instance.new("PitchShiftSoundEffect")
-shift.Octave = 0.5
+shift.Octave = 1
 shift.Parent = scare
 
 local distort = Instance.new("DistortionSoundEffect")
 distort.Parent = scare
-distort.Level = 0.75
-			
+distort.Level = 1
+
+local v2 = Random.new()
+SizeValue.Value = -0.15
+local v4 = math.random(1, 2)
+scare:Play()
+for v5 = 1, 30 do
+	local v6 = math.random(1, 3)
+	if v6 == 1 then
+		Background.BackgroundColor3 = Color3.new(0, 0, 0)
+		Face.ImageColor3 = Color3.new(1, 1, 1)
+	end
+	if v6 == 2 then
+		Background.BackgroundColor3 = Color3.new(0.596078, 0.596078, 0.596078)
+		Face.ImageColor3 = Color3.new(0.858824, 1, 0.796078)
+	end
+	if v6 == 3 then
+		Background.BackgroundColor3 = Color3.new(1, 1, 1)
+		Face.ImageColor3 = Color3.new(0.647059, 0.647059, 0.647059)
+	end
+	Face.Position = UDim2.new(v2:NextNumber(0.4, 0.6), 0,v2:NextNumber(0.4, 0.6), 0)
+	Face.Size = ImageLabel.Size + UDim2.new(SizeValue.Value, 0, SizeValue.Value, 0)
+	Face.Rotation = math.random(-50, 50)
+	SizeValue.Value = SizeValue.Value + 0.02
+	wait(0)
+end
+ImageLabel.ImageColor3 = Color3.new(1, 1, 1)
+Background.BackgroundColor3 = Color3.new(0, 0, 0)
+JumpscareGui.Enabled = false
+
 	end
 	end)
 
