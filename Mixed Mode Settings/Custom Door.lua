54647c5eb5719bffa1e8a49a9f6a6ed47209ffa6
Timofey2339 <kirpatimofey@gmail.com>
2026-03-13 23:21:18 +0200

    Update Custom Door.lua

--- Mixed Mode Settings/Custom Door.lua
+++ Mixed Mode Settings/Custom Door.lua
@@ -4,17 +4,26 @@ local function changeDoor(room)
 	local door = room:FindFirstChild("Door")
 	if door then
 		
-		local NewDoor = game:GetObjects("rbxassetid://12245371479")[1]
+		local NewDoor = game:GetObjects("rbxassetid://81616085996734")[1]
 		NewDoor.Parent = room
 		
 		-- робимо стару дверь невидимою
 		for _,v in pairs(door:GetDescendants()) do
 			if v:IsA("BasePart") then
 				v.Transparency = 1
+				v.CanCollide = false
+			end
+			
+			if v:IsA("Decal") or v:IsA("Texture") then
+				v.Transparency = 1
+			end
+			
+			if v:IsA("SurfaceGui") or v:IsA("BillboardGui") then
+				v.Enabled = false
 			end
 		end
 		
-		-- слідкуємо за позицією
+		-- нова дверь повторює позицію старої
 		task.spawn(function()
 			while door.Parent do
 				NewDoor:PivotTo(door:GetPivot())
@@ -25,13 +34,12 @@ local function changeDoor(room)
 	end
 end
 
--- для нових кімнат
 rooms.ChildAdded:Connect(function(room)
 	task.wait(0.5)
 	changeDoor(room)
 end)
 
--- для існуючих
 for _,room in pairs(rooms:GetChildren()) do
 	changeDoor(room)
 end
+
