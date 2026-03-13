21c13615b218dd5c89e3b478344355c4a1117d2c
Timofey2339 <kirpatimofey@gmail.com>
2026-03-13 22:30:54 +0200

    Update Custom Door.lua

--- Mixed Mode Settings/Custom Door.lua
+++ Mixed Mode Settings/Custom Door.lua
@@ -1,1 +1,11 @@
+local room = workspace.CurrentRooms[game.ReplicatedStorage.GameData.LatestRoom.Value]
+local oldDoor = room:FindFirstChild("Door")
 
+local NewDoor = game:GetObjects("rbxassetid://12245371479")[1]
+
+if oldDoor then
+	NewDoor.CFrame = oldDoor.CFrame
+	oldDoor:Destroy()
+end
+
+NewDoor.Parent = room
