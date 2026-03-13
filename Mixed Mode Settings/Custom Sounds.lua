0571b3d1c4c1cf8328b20925a85d7d4fce4b3f11
Timofey2339 <kirpatimofey@gmail.com>
2026-03-13 22:22:48 +0200

    Update Custom Sounds.lua

--- Mixed Mode Settings/Custom Sounds.lua
+++ Mixed Mode Settings/Custom Sounds.lua
@@ -1,5 +1,7 @@
-while true do
-        wait(0.01)
-        workspace.CurrentRooms[game.ReplicatedStorage.GameData.LatestRoom.Value].Door.Door.Open.Volume = 3
+coroutine.wrap(function()
+    while true do
+        wait()
+            workspace.CurrentRooms[game.ReplicatedStorage.GameData.LatestRoom.Value].Door.Door.Open.Volume = 3
         workspace.CurrentRooms[game.ReplicatedStorage.GameData.LatestRoom.Value].Door.Door.Open.SoundId = "rbxassetid://112999617701939"
-end
+      end
+end)()
