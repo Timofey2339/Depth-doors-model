7537c853a69279e47f7b3f5a18c2d45afc082f78
Timofey2339 <kirpatimofey@gmail.com>
2026-03-14 20:37:10 +0200

    Update Fanmade Dread.lua

--- Fanmade Dread.lua
+++ Fanmade Dread.lua
@@ -69,3 +69,70 @@ sound3:Play()
 game.TweenService:Create(Warneffect,TweenInfo.new(15),{TintColor = Color3.fromRGB(255, 255, 255),Saturation = 0, Contrast = 0}):Play()
 wait(9)
 sound3:Destroy()
+local spawner = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Entity%20Spawner/V2/Source.lua"))()	
+local entity = spawner.Create({
+        Entity = {
+            Name = "Fanmade Dread",
+            Asset = "rbxassetid://17056719213",
+            HeightOffset = 0
+        },
+        Lights = {
+            Flicker = { Enabled = false, Duration = 6},
+            Shatter = false, Repair = false
+        },
+        Earthquake = { Enabled = false },
+        CameraShake = { Enabled = true, Range = 100, Values = {5, 5, 0.1, 1} },
+                Jumpscare = {
+            false, -- Enabled
+            {
+                Image1 = "rbxassetid://10483855823", -- A-60 Jumpscare Image
+                Image2 = "rbxassetid://11360803115",
+                Shake = true,
+                Sound1 = {18459521002, 1}, -- Jumpscare sound
+                Sound2 = {18459521002, 1},
+                Flashing = {true, Color3.fromRGB(255, 0, 0)},
+                Tease = {true, Min = 1, Max = 3},
+            },
+        },
+        Movement = { Speed = 300, Delay = 2, Reversed = false },
+        Rebounding = { Enabled = true, Type = "Ambush", Min = 1, Max = 1, Delay = 1.0 },
+        Damage = { Enabled = true, Range = 999, Amount = 999 },
+        Crucifixion = { Enabled = true, Range = 40, Resist = false, Break = true },
+        Death = {
+            Type = "Curious",
+            Hints = {"You died by Fanmade Dread", "If clock stop ticking", "all what you needs its hide","Bye Bye"}, 
+            Cause = "Fanmade Dread"
+        }
+    })
+
+    entity:SetCallback("OnSpawned", function()
+wait(5)
+        SetAtmosphere(Color3.fromRGB(85, 150, 255), 0.5)
+        local model = game.Workspace:FindFirstChild("Fanmade Dread")
+        if model then
+            for _, v in pairs(model:GetDescendants()) do
+                if v:IsA("Sound") then
+                    v.SoundId = "rbxassetid://111930358743197"
+                    v.Volume = 2
+                    v.PlaybackSpeed = 1
+                elseif v:IsA("BasePart") then
+                    v.Color = Color3.fromRGB(0, 0, 80)
+                    v.Material = Enum.Material.Neon
+                    local light = Instance.new("PointLight", v)
+                    light.Color = Color3.fromRGB(0, 255, 255)
+                    light.Range = 60; light.Brightness = 6; light.Shadows = true
+                elseif v:IsA("Decal") then
+                    v.Texture = "rbxassetid://11118765532"
+                end
+            end
+        end
+    end)
+
+entity:SetCallback("OnDamagePlayer", function(newHealth)
+	if newHealth == 0 then
+
+	end
+	end)
+
+    entity:SetCallback("OnDespawned", function() ClearAtmosphere() end)
+    entity:Run()
