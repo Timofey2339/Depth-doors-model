14404efd45ea778af5f239e01154bb4e4f31e6e4
Timofey2339 <kirpatimofey@gmail.com>
2026-03-14 13:31:38 +0200

    Create Change Eyes texture.lua

---
+++ Mixed Mode Settings/Change Eyes texture.lua
@@ -0,0 +1,31 @@
+workspace.ChildAdded:Connect(function(child)
+    if child.Name == "Eyes" then
+        
+        child.Core.Attachment.Angry.Texture = "rbxassetid://0"
+        child.Core.Attachment.Bite.Texture = "rbxassetid://0"
+        child.Core.Attachment.Spark.Texture = "rbxassetid://0"
+        child.Core.Attachment.EyesParticle.Texture = "rbxassetid://94041916253312"
+        child.Core.Attachment.EyesParticle.Size = NumberSequence.new(10)
+
+        child.Core.Ambience.Volume = 4
+        child.Core.Ambience.PlaybackSpeed = 0.2
+
+        child.Core.Attack.Volume = 4
+        child.Core.Attack.PlaybackSpeed = 0.6
+
+        child.Core.Attack.Volume = 4
+        child.Core.Attack.PlaybackSpeed = 0.6
+
+        child.Core.Repent.Volume = 4
+        child.Core.Repent.PlaybackSpeed = 0.6
+
+        child.Core.Initiate.Volume = 4
+        child.Core.Initiate.PlaybackSpeed = 0.6
+
+        child.Core.Teleport.Volume = 4
+        child.Core.Teleport.PlaybackSpeed = 0.6
+
+        child.Core.Scream.Volume = 4
+        child.Core.Scream.PlaybackSpeed = 0.3
+    end
+end)
