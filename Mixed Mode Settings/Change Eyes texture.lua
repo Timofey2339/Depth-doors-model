874938eeea11ef8b8bd245eb7aee20da139939cf
Timofey2339 <kirpatimofey@gmail.com>
2026-03-14 13:47:57 +0200

    Update Change Eyes texture.lua

--- Mixed Mode Settings/Change Eyes texture.lua
+++ Mixed Mode Settings/Change Eyes texture.lua
@@ -9,29 +9,33 @@ workspace.ChildAdded:Connect(function(child)
         attachment:WaitForChild("Spark").Texture = "rbxassetid://0"
 
         local eyesParticle = attachment:WaitForChild("EyesParticle")
-        eyesParticle.Texture = "rbxassetid://94041916253312"
         eyesParticle.Size = NumberSequence.new(10)
+
+        task.spawn(function()
+            while child.Parent do
+                eyesParticle.Texture = "rbxassetid://94041916253312"
+                task.wait(0.05)
+            end
+        end)
 
         local ambience = core:WaitForChild("Ambience")
         ambience.Volume = 4
         ambience.PlaybackSpeed = 0.2
 
-        child.Core.Attack.Volume = 4
-        child.Core.Attack.PlaybackSpeed = 0.6
+        core.Attack.Volume = 4
+        core.Attack.PlaybackSpeed = 0.6
 
-        child.Core.Attack.Volume = 4
-        child.Core.Attack.PlaybackSpeed = 0.6
+        core.Repent.Volume = 4
+        core.Repent.PlaybackSpeed = 0.6
 
-        child.Core.Repent.Volume = 4
-        child.Core.Repent.PlaybackSpeed = 0.6
+        core.Initiate.Volume = 4
+        core.Initiate.PlaybackSpeed = 0.6
 
-        child.Core.Initiate.Volume = 4
-        child.Core.Initiate.PlaybackSpeed = 0.6
+        core.Teleport.Volume = 4
+        core.Teleport.PlaybackSpeed = 0.6
 
-        child.Core.Teleport.Volume = 4
-        child.Core.Teleport.PlaybackSpeed = 0.6
-
-        child.Core.Scream.Volume = 4
-        child.Core.Scream.PlaybackSpeed = 0.3
+        core.Scream.Volume = 4
+        core.Scream.PlaybackSpeed = 0.3
     end
+end)
 end)
