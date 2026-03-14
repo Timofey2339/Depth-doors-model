a25ea0b7a276543d2c1c85b2de1f76a71b57e5a2
Timofey2339 <kirpatimofey@gmail.com>
2026-03-14 22:43:56 +0200

    Update Watcher.lua

--- Watcher.lua
+++ Watcher.lua
@@ -1,142 +1,73 @@
 local Watcher = Instance.new("Model")
-Watcher.Parent = workspace
 Watcher.Name = "WatcherEntity"
+Watcher.Parent = workspace
 
-local v = game.Players.LocalPlayer
+local player = game.Players.LocalPlayer
+local hum = player.Character:WaitForChild("Humanoid")
 local enableDamage = true
 local no = false
 
-local damage1 = Instance.new("Sound")
-damage1.SoundId = "rbxassetid://137162043074149"
-damage1.PlaybackSpeed = 1
-damage1.Volume = 0.7
+-- звуки
+local sounds = {}
+for i = 1,3 do
+	local s = Instance.new("Sound")
+	s.SoundId = "rbxassetid://137162043074149"
+	s.PlaybackSpeed = 1
+	s.Volume = 0.7
+	s.Parent = Watcher
+	table.insert(sounds,s)
+end
 
-local damage2 = Instance.new("Sound")
-damage2.SoundId = "rbxassetid://137162043074149"
-damage2.PlaybackSpeed = 1
-damage2.Volume = 0.7
-
-local damage3 = Instance.new("Sound")
-damage3.SoundId = "rbxassetid://137162043074149"
-damage3.PlaybackSpeed = 1
-damage3.Volume = 0.7
-
+-- камера
 local CameraShaker = require(game.ReplicatedStorage.CameraShaker)
 local camera = workspace.CurrentCamera
-
-local camShake = CameraShaker.new(Enum.RenderPriority.Camera.Value,function(shakeCf)
-	camera.CFrame = camera.CFrame * shakeCf
+local camShake = CameraShaker.new(Enum.RenderPriority.Camera.Value,function(cf)
+	camera.CFrame = camera.CFrame * cf
 end)
 
-local currentLoadedRoom = workspace.CurrentRooms[game.ReplicatedStorage.GameData.LatestRoom.Value]
+-- кімната
+local room = workspace.CurrentRooms[game.ReplicatedStorage.GameData.LatestRoom.Value]
+local spawnPart
+if room:FindFirstChild("Nodes") then
+	local nodes = room.Nodes:GetChildren()
+	spawnPart = nodes[math.floor(#nodes/2)]
+else
+	spawnPart = room:FindFirstChild(room.Name)
+end
+
+-- Part очей
 local eyes = game:GetObjects("rbxassetid://79312363226377")[1]
+eyes.Parent = Watcher
+eyes.CFrame = spawnPart.CFrame * CFrame.new(0,5,0)
 
+-- якщо нова кімната
 workspace.CurrentRooms.ChildAdded:Connect(function()
-	if Watcher.Parent then
-		Watcher:Destroy()
-	end
 	enableDamage = false
+	Watcher:Destroy()
 end)
 
-local num = 0
-if currentLoadedRoom:FindFirstChild("Nodes") then
-	num = math.floor(#currentLoadedRoom.Nodes:GetChildren()/2)
-end
-
-local spawnPart = (num == 0 and currentLoadedRoom[currentLoadedRoom.Name] or currentLoadedRoom.Nodes[num])
-
-if eyes then
-	eyes.Parent = Watcher
-	eyes:PivotTo(spawnPart.CFrame * CFrame.new(0,5,0))
-end
-
-damage1.Parent = Watcher
-damage2.Parent = Watcher
-damage3.Parent = Watcher
-
-local sounds = {damage1,damage2,damage3}
-
-local hum = v.Character:WaitForChild("Humanoid")
-
-if hum.Health <= 0 then
-	no = true
-end
-
+-- Damage цикл
 while enableDamage do
-	if not Watcher.Parent then break end
+	if not eyes.Parent then break end
 
-	local _,found = workspace.CurrentCamera:WorldToScreenPoint(eyes:GetPivot().Position)
-
-	if not found then
-		if v.Character and not v.Character:GetAttribute("Hiding") then
-
-			if workspace.CurrentRooms:FindFirstChild("51") or workspace:FindFirstChild("SeekMoving") then
-				return
-			end
-
+	local _,visible = camera:WorldToScreenPoint(eyes.Position)
+	if not visible then
+		if player.Character and not player.Character:GetAttribute("Hiding") then
 			camShake:Start()
 			camShake:ShakeOnce(5,15,0.1,1)
 
 			hum.Health -= 3
-
 			if not no then
 				sounds[math.random(1,#sounds)]:Play()
 			end
 
 			if hum.Health <= 0 then
-				if not no then
-					game.ReplicatedStorage.GameStats["Player_"..v.Name].Total.DeathCause.Value = "Watcher"
-					no = true
-					task.wait(1.8)
-				end
+				no = true
+				game.ReplicatedStorage.GameStats["Player_"..player.Name].Total.DeathCause.Value = "Watcher"
+				task.wait(1.8)
 			end
 		end
 	end
 
-	task.wait(0.5)
-end
-   num = math.floor(#currentLoadedRoom.Nodes:GetChildren()/2)
-end
-
-eyes.CFrame=(
-	num==0 and currentLoadedRoom[currentLoadedRoom.Name] or currentLoadedRoom.Nodes[num]
-).CFrame+Vector3.new(0,5,0)
- 
-eyes.Parent=Watcher
-damage1.Parent=Watcher
-damage2.Parent=Watcher
-damage3.Parent=Watcher
-local mom = {damage1, damage2, damage3}
-
-if game.Players.LocalPlayer.Character.Humanoid.Health<=0 then
-   no = true
-end
- 
-local hum=game:GetService("Players").LocalPlayer.Character:FindFirstChildOfClass("Humanoid")
-while true and enableDamage do
-if not Watcher then break end
-	local _,found=workspace.CurrentCamera:WorldToScreenPoint(eyes.Position)
-	if not found then
-	if v.Character ~= nil and not v.Character:GetAttribute("Hiding") then
-if workspace.CurrentRooms:FindFirstChild("51") or workspace:FindFirstChild("SeekMoving")  then
-return
-end
-
-camShake:Start()
-camShake:ShakeOnce(5, 15, 0.1, 1)
-		hum.Health-=3
-          if no == false then
-		mom[math.random(1, #mom)]:Play()
-          end
-		if hum.Health<=0 then
-		if no == false then
-			game:GetService("ReplicatedStorage").GameStats["Player_".. game.Players.LocalPlayer.Name].Total.DeathCause.Value = "Watcher"
-			no = true
-			wait(1.8)
-			
-		end
-		end
-	end
-	end
 	task.wait(0.5)
 end
