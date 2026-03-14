db4ca9b0d3a2e625a8eef57c529dbdd8a2e6f4bd
Timofey2339 <kirpatimofey@gmail.com>
2026-03-15 01:04:24 +0200

    Update Watcher.lua

--- Watcher.lua
+++ Watcher.lua
@@ -1,73 +1,58 @@
-local Watcher = Instance.new("Model")
-Watcher.Name = "WatcherEntity"
-Watcher.Parent = workspace
-
 local player = game.Players.LocalPlayer
-local hum = player.Character:WaitForChild("Humanoid")
-local enableDamage = true
-local no = false
+local char = player.Character or player.CharacterAdded:Wait()
+local hum = char:WaitForChild("Humanoid")
 
--- звуки
-local sounds = {}
-for i = 1,3 do
-	local s = Instance.new("Sound")
-	s.SoundId = "rbxassetid://137162043074149"
-	s.PlaybackSpeed = 1
-	s.Volume = 0.7
-	s.Parent = Watcher
-	table.insert(sounds,s)
+local rooms = workspace:WaitForChild("CurrentRooms")
+local gameData = game.ReplicatedStorage:WaitForChild("GameData")
+
+local latestRoomValue = gameData:WaitForChild("LatestRoom")
+
+-- завантажуємо модель
+local entity = game:GetObjects("rbxassetid://79312363226377")[1]
+entity.Parent = workspace
+
+-- звук
+local sound = Instance.new("Sound")
+sound.SoundId = "rbxassetid://137162043074149"
+sound.Volume = 1
+sound.Parent = workspace
+
+-- знайти кімнату
+local room = rooms:FindFirstChild(tostring(latestRoomValue.Value))
+
+if not room then
+	warn("Room not found")
+	return
 end
+
+-- позиція
+entity:PivotTo(room:GetPivot() * CFrame.new(0,5,0))
 
 -- камера
-local CameraShaker = require(game.ReplicatedStorage.CameraShaker)
 local camera = workspace.CurrentCamera
-local camShake = CameraShaker.new(Enum.RenderPriority.Camera.Value,function(cf)
-	camera.CFrame = camera.CFrame * cf
-end)
 
--- кімната
-local room = workspace.CurrentRooms[game.ReplicatedStorage.GameData.LatestRoom.Value]
-local spawnPart
-if room:FindFirstChild("Nodes") then
-	local nodes = room.Nodes:GetChildren()
-	spawnPart = nodes[math.floor(#nodes/2)]
-else
-	spawnPart = room:FindFirstChild(room.Name)
-end
+-- запам’ятовуємо кімнату
+local currentRoomNumber = latestRoomValue.Value
 
--- Part очей
-local eyes = game:GetObjects("https://assetdelivery.roblox.com/v1/asset/?id=79312363226377")[1]
-eyes.Parent = Watcher
-eyes.CFrame = spawnPart.CFrame * CFrame.new(0,5,0)
-
--- якщо нова кімната
-workspace.CurrentRooms.ChildAdded:Connect(function()
-	enableDamage = false
-	Watcher:Destroy()
-end)
-
--- Damage цикл
-while enableDamage do
-	if not eyes.Parent then break end
-
-	local _,visible = camera:WorldToScreenPoint(eyes.Position)
+while task.wait(0.4) do
+	
+	-- якщо змінилась кімната → видаляємо ентіті
+	if latestRoomValue.Value ~= currentRoomNumber then
+		entity:Destroy()
+		break
+	end
+	
+	if not entity.Parent then break end
+	
+	local pos = entity:GetPivot().Position
+	
+	local _,visible = camera:WorldToScreenPoint(pos)
+	
 	if not visible then
-		if player.Character and not player.Character:GetAttribute("Hiding") then
-			camShake:Start()
-			camShake:ShakeOnce(5,15,0.1,1)
-
-			hum.Health -= 3
-			if not no then
-				sounds[math.random(1,#sounds)]:Play()
-			end
-
-			if hum.Health <= 0 then
-				no = true
-				game.ReplicatedStorage.GameStats["Player_"..player.Name].Total.DeathCause.Value = "Watcher"
-				task.wait(1.8)
-			end
+		if hum.Health > 0 then
+			hum.Health -= 4
+			sound:Play()
 		end
 	end
-
-	task.wait(0.5)
+	
 end
