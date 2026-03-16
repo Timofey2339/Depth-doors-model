d56b788acef4f6c65ada8aef6ef913c8d6d39ec3
Timofey2339 <kirpatimofey@gmail.com>
2026-03-16 18:12:07 +0200

    Update Rebound Raw.lua

--- Rebound Raw.lua
+++ Rebound Raw.lua
@@ -104,7 +104,7 @@ game.ReplicatedStorage.GameData.LatestRoom.Changed:Connect(function()
 
 	local sound = Instance.new("Sound")
 	sound.Parent = workspace
-	sound.SoundId = "rbxassetid://71840455801134"
+	sound.SoundId = "rbxassetid://95181878959735"
 	sound.Volume = 5
 
 	local pitch = Instance.new("PitchShiftSoundEffect")
@@ -128,17 +128,50 @@ game.ReplicatedStorage.GameData.LatestRoom.Changed:Connect(function()
 
 	task.wait(0.04)
 
-	local spawner = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Entity%20Spawner/V2/Source.lua"))()
+local spawner = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Entity%20Spawner/V2/Source.lua"))()	
+local entity = spawner.Create({
+        Entity = {
+            Name = "Rebound",
+            Asset = "rbxassetid://95181878959735",
+            HeightOffset = 0
+        },
+        Lights = {
+            Flicker = { Enabled = false, Duration = 6},
+            Shatter = false, Repair = false
+        },
+        Earthquake = { Enabled = false },
+        CameraShake = { Enabled = true, Range = 100, Values = {20, 20, 0.1, 1} },
+        Movement = { Speed = 100, Delay = 2, Reversed = true },
+        Rebounding = { Enabled = false, Type = "Ambush", Min = 1, Max = 1, Delay = 1.0 },
+        Damage = { Enabled = true, Range = 40, Amount = 125 },
+        Crucifixion = { Enabled = false, Range = 40, Resist = false, Break = true },
+        Death = {
+            Type = "Curious",
+            Hints = {"You died by Rebound", "if you your Screen blue hide", "and now","Bye Bye"}, 
+            Cause = "Rebound"
+        }
+    })
 
-	local entity = spawner.Create({
-		Entity = {
-			Name = "Rebound",
-			Asset = "rbxassetid://95181878959735",
-			HeightOffset = 0
-		}
-	})
-
-	entity:Run()
-
+    entity:SetCallback("OnSpawned", function()
+        SetAtmosphere(Color3.fromRGB(100, 150, 255), 0.5)
+        local model = game.Workspace:FindFirstChild("Rebound")
+        if model then
+            for _, v in pairs(model:GetDescendants()) do
+                if v:IsA("Sound") then
+                    v.SoundId = "rbxassetid://111930358743197"
+                    v.Volume = 2
+                    v.PlaybackSpeed = 1
+                elseif v:IsA("BasePart") then
+                    v.Color = Color3.fromRGB(0, 0, 80)
+                    v.Material = Enum.Material.Neon
+                    local light = Instance.new("PointLight", v)
+                    light.Color = Color3.fromRGB(0, 255, 255)
+                    light.Range = 60; light.Brightness = 6; light.Shadows = true
+                elseif v:IsA("Decal") then
+                    v.Texture = "rbxassetid://11118765532"
+                end
+            end
+        end
+    end)
 end)
 
