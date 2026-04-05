947e2432f3a8b6317c2a158490fd1b7e7017646d
Timofey2339 <kirpatimofey@gmail.com>
2026-04-06 02:29:13 +0300

    Implement entity visibility check and health damage
    Added visibility check for entity and health reduction logic.

--- Watcher.lua
+++ Watcher.lua
@@ -1,17 +1,13 @@
-local player = game.Players.LocalPlayer
+І у цьому скрипті local player = game.Players.LocalPlayer
 local char = player.Character or player.CharacterAdded:Wait()
 local hum = char:WaitForChild("Humanoid")
 
 local rooms = workspace:WaitForChild("CurrentRooms")
 local gameData = game.ReplicatedStorage:WaitForChild("GameData")
+
 local latestRoomValue = gameData:WaitForChild("LatestRoom")
 
 local entity = game:GetObjects("rbxassetid://79312363226377")[1]
-if not entity then
-	warn("Entity not loaded!")
-	return
-end
-
 entity.Parent = workspace
 
 local sound = Instance.new("Sound")
@@ -20,6 +16,7 @@ sound.Volume = 1
 sound.Parent = workspace
 
 local room = rooms:FindFirstChild(tostring(latestRoomValue.Value))
+
 if not room then
 	warn("Room not found")
 	return
@@ -35,6 +32,43 @@ while task.wait(0.4) do
 	if latestRoomValue.Value ~= currentRoomNumber then
 		entity:Destroy()
 		break
+	end
+	
+	if not entity.Parent then break end
+	
+	local pos = entity:GetPivot().Position
+	
+	local _, visible = camera:WorldToScreenPoint(pos)
+	
+	local origin = camera.CFrame.Position
+	local direction = pos - origin
+	
+	local rayParams = RaycastParams.new()
+	rayParams.FilterDescendantsInstances = {char, entity}
+	rayParams.FilterType = Enum.RaycastFilterType.Blacklist
+	
+	local result = workspace:Raycast(origin, direction, rayParams)
+	
+	local canSee = false
+	
+	if visible then
+		if result then
+			if result.Instance:IsDescendantOf(entity) then
+				canSee = true
+			end
+		else
+			canSee = true
+		end
+	end
+	
+	if not canSee then
+		if hum.Hea lth > 0 then
+			hum.Health -= 5
+			sound:Play()
+		end
+	end
+	
+end	break
 	end
 	
 	if not entity.Parent then break end
