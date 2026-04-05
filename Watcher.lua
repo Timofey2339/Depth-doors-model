130fdb88a71e545ecc31781d6182b864af1f8065
Timofey2339 <kirpatimofey@gmail.com>
2026-04-05 20:26:25 +0300

    Adjust health decrement for entity visibility
    Updated health decrement from 0.5 to 5 when entity is not visible.

--- Watcher.lua
+++ Watcher.lua
@@ -7,17 +7,14 @@ local gameData = game.ReplicatedStorage:WaitForChild("GameData")
 
 local latestRoomValue = gameData:WaitForChild("LatestRoom")
 
--- завантажуємо модель
 local entity = game:GetObjects("rbxassetid://79312363226377")[1]
 entity.Parent = workspace
 
--- звук
 local sound = Instance.new("Sound")
 sound.SoundId = "rbxassetid://137162043074149"
 sound.Volume = 1
 sound.Parent = workspace
 
--- знайти кімнату
 local room = rooms:FindFirstChild(tostring(latestRoomValue.Value))
 
 if not room then
@@ -25,18 +22,13 @@ if not room then
 	return
 end
 
--- позиція
 entity:PivotTo(room:GetPivot() * CFrame.new(0,0,-25))
 
--- камера
 local camera = workspace.CurrentCamera
-
--- запам’ятовуємо кімнату
 local currentRoomNumber = latestRoomValue.Value
 
 while task.wait(0.4) do
 	
-	-- якщо змінилась кімната → видаляємо ентіті
 	if latestRoomValue.Value ~= currentRoomNumber then
 		entity:Destroy()
 		break
@@ -46,11 +38,32 @@ while task.wait(0.4) do
 	
 	local pos = entity:GetPivot().Position
 	
-	local _,visible = camera:WorldToScreenPoint(pos)
+	local _, visible = camera:WorldToScreenPoint(pos)
 	
-	if not visible then
+	local origin = camera.CFrame.Position
+	local direction = pos - origin
+	
+	local rayParams = RaycastParams.new()
+	rayParams.FilterDescendantsInstances = {char, entity}
+	rayParams.FilterType = Enum.RaycastFilterType.Blacklist
+	
+	local result = workspace:Raycast(origin, direction, rayParams)
+	
+	local canSee = false
+	
+	if visible then
+		if result then
+			if result.Instance:IsDescendantOf(entity) then
+				canSee = true
+			end
+		else
+			canSee = true
+		end
+	end
+	
+	if not canSee then
 		if hum.Health > 0 then
-			hum.Health -= 0.5
+			hum.Health -= 5
 			sound:Play()
 		end
 	end
