4cf42303430855e01b4bac0ebbbf4b2910516f13
Timofey2339 <kirpatimofey@gmail.com>
2026-04-17 18:15:46 +0300

    Implement jumpscare GUI with sound and visual effects
    This script creates a jumpscare GUI with sound effects and visual effects that change colors and sizes randomly.

---
+++ Jumpscare/Watcher Jumpscare.lua
@@ -0,0 +1,75 @@
+		-- UI Construction
+    local JumpscareGui = Instance.new("ScreenGui")
+    local Background = Instance.new("Frame")
+    local Face = Instance.new("ImageLabel")
+	local SizeValue = Instance.new("NumberValue")
+
+	SizeValue.Name = "SizeValue"
+	SizeValue.Value = -0.07
+
+    JumpscareGui.Name = "Jumpscare"
+    JumpscareGui.IgnoreGuiInset = true
+    JumpscareGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+    JumpscareGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+
+    Background.Name = "Frame"
+    Background.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+    Background.BorderSizePixel = 0
+    Background.Size = UDim2.new(1, 0, 1, 0)
+    Background.ZIndex = 50
+
+    Face.Name = "ImageLabel"
+    Face.AnchorPoint = Vector2.new(0.5, 0.5)
+    Face.BackgroundTransparency = 1
+    Face.Position = UDim2.new(0.5, 0, 0.5, 0)
+    Face.ResampleMode = Enum.ResamplerMode.Pixelated
+    Face.Size = UDim2.new(0.25, 0, 0.3, 0)
+    Face.Image = "rbxassetid://13335477221"
+
+local scare = Instance.new("Sound")
+scare.Parent = Background
+scare.Name = "Jumpscare"
+scare.SoundId = "rbxassetid://6459610652"
+scare.PlaybackSpeed = 1
+scare.Volume = 10
+
+local shift = Instance.new("PitchShiftSoundEffect")
+shift.Octave = 1
+shift.Parent = scare
+
+local distort = Instance.new("DistortionSoundEffect")
+distort.Parent = scare
+distort.Level = 0.4
+
+    Background.Parent = JumpscareGui
+    Face.Parent = Background
+	SizeValue.Parent = Background
+
+local v2 = Random.new()
+SizeValue.Value = -0.07
+scare:Play()
+local v4 = math.random(1, 2)
+for v5 = 1, 7 do
+	local randint = math.random(1, 3)
+	if randint == 1 then
+		Background.BackgroundColor3 = Color3.fromRGB(245, 205, 48)
+		Background.BorderColor3 = Color3.fromRGB(0, 0, 0)
+		Face.ImageColor3 = Color3.new(0, 0, 0)
+	elseif randint == 2 then
+		Background.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+		Background.BorderColor3 = Color3.fromRGB(0, 0, 0)
+		Face.ImageColor3 = Color3.new(1, 0, 0)
+	elseif randint == 3 then
+		Background.BackgroundColor3 = Color3.fromRGB(218, 133, 65)
+		Background.BorderColor3 = Color3.fromRGB(218, 133, 65)
+		Face.ImageColor3 = Color3.new(0.67451, 0.376471, 0.376471)
+	end
+	Face.Position = UDim2.new(v2:NextNumber(0.4, 0.6), 0, v2:NextNumber(0.4, 0.6), 0)
+	Face.Size += UDim2.new(0, SizeValue.Value, 0, SizeValue.Value)
+	Face.Rotation = math.random(-25, 25)
+	SizeValue.Value += 2
+	wait(0)
+end
+Face.ImageColor3 = Color3.new(1, 1, 1)
+Background.BackgroundColor3 = Color3.new(0, 0, 0)
+JumpscareGui.Enabled = false
