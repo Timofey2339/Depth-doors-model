b040dd79b01472b8634d11a1bede63464ecc1c38
Timofey2339 <kirpatimofey@gmail.com>
2026-04-17 22:55:54 +0300

    Refactor coroutine and sound initialization
    Removed redundant coroutine for room change detection and adjusted sound initialization timing.

--- Rebound Raw.lua
+++ Rebound Raw.lua
@@ -8,6 +8,15 @@ local pitch = Instance.new("PitchShiftSoundEffect")
 pitch.Parent = sound 
 pitch.Octave = 1
 sound:Play()
+coroutine.wrap(function()
+    while true do
+        wait(0.1)
+        game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+        if workspace:FindFirstChild("SeekMovingNewClone") or workspace.CurrentRooms:FindFirstChild("50") then
+            return
+        end
+    end
+end)
 local Reboundcolor = Instance.new("ColorCorrectionEffect",game.Lighting) game.Debris:AddItem(Reboundcolor,24) Reboundcolor.Name = "Warn"
 Reboundcolor.TintColor = Color3.fromRGB(65, 138, 255) Reboundcolor.Saturation = -0.7 Reboundcolor.Contrast = 0.2
 game.TweenService:Create(Reboundcolor,TweenInfo.new(15),{TintColor = Color3.fromRGB(255, 255, 255),Saturation = 0, Contrast = 0}):Play()
@@ -16,15 +25,6 @@ local camara = game.Workspace.CurrentCamera
 local camShake = CameraShaker.new(Enum.RenderPriority.Camera.Value, function(shakeCf) camara.CFrame = camara.CFrame * shakeCf end)
 camShake:Start() 
 camShake:ShakeOnce(10,3,0.1,6,2,0.5) 
-coroutine.wrap(function()
-    while true do
-        wait(0.1)
-        game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
-          if workspace:FindFirstChild("SeekMovingNewClone") or workspace.CurrentRooms:FindFirstChild("50") then
-          return
-            end
-        end)
-	
 wait(4.96)
 local sound = Instance.new("Sound")
 sound.Parent = workspace
