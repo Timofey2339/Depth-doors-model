62072d101a34e175bed7a734938247f197465327
Timofey2339 <kirpatimofey@gmail.com>
2026-07-03 11:10:42 +0300

    Add GUI with button to log.lua

---
+++ Update log.lua
@@ -0,0 +1,6 @@
+local gui = Instance.new("ScreenGui")
+gui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+
+local button = Instance.new("Textbutton")
+button.Size =
+button.Parent = gui
