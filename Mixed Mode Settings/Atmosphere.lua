af89355e588048748d1ae544b7e9adabd8938f00
Timofey2339 <kirpatimofey@gmail.com>
2026-07-06 07:15:59 +0300

    Update Atmosphere.lua

--- Mixed Mode Settings/Atmosphere.lua
+++ Mixed Mode Settings/Atmosphere.lua
@@ -1,1 +1,11 @@
-
+local soundLight = Instance.new("Sound")
+soundLight.Parent = game.Workspace
+soundLight.SoundId = "rbxasset://136848722764245
+soundLight.Volume = 3
+soundLight:Play()
+wait(0.01)
+game:GetService("TweenService"):Create(game.Lighting, TweenInfo.new(0.01), {FogEnd = 0}):Play()
+game:GetService("TweenService"):Create(game.Lighting, TweenInfo.new(0.01), {FogStart = 0}):Play()
+wait(0.01)
+game:GetService("TweenService"):Create(game.Lighting, TweenInfo.new(2), {FogEnd = 50}):Play()
+game:GetService("TweenService"):Create(game.Lighting, TweenInfo.new(2), {FogStart = 10}):Play()
