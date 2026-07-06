12195a87db727b0f543562a022ecd35b47dad2b4
Timofey2339 <kirpatimofey@gmail.com>
2026-07-06 05:27:56 +0300

    Update version log with new changes

--- Update log.lua
+++ Update log.lua
@@ -31,7 +31,7 @@ TextUpdate.TextSize = 8
 TextUpdate.RichText = true
 TextUpdate.TextWrapped = true
 TextUpdate.TextScaled = true
-TextUpdate.Text = "Version 1.7, Monoxide has been updated by little, Greg got changed, Depth has been deleted, added new entity called Rambey"
+TextUpdate.Text = "Version 1.7, Monoxide has been updated by little, Greg got changed, Depth has been deleted, added new entity called Rambey, Mixed mode changed to Particulate Mode"
 
 local buttonClose = Instance.new("TextButton") 
 buttonClose.BackgroundTransparency = 0.5
