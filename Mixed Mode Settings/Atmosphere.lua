705a7d74a89d988286d09c9d595b710ffab504ca
Timofey2339 <kirpatimofey@gmail.com>
2026-07-06 07:25:54 +0300

    Refactor sound instance creation and properties

--- Mixed Mode Settings/Atmosphere.lua
+++ Mixed Mode Settings/Atmosphere.lua
@@ -1,8 +1,10 @@
-local soundLight = Instance.new("Sound")
-soundLight.Parent = game.Workspace
-soundLight.SoundId = "rbxassetid://136848722764245
-soundLight.Volume = 3
-soundLight:Play()
+local sound = Instance.new("Sound")
+sound.Parent = workspace
+sound.SoundId = "rbxassetid://136848722764245"
+sound.Volume = 5
+sound.Looped = false
+sound.PlaybackSpeed = 1
+sound:Play()
 wait(0.01)
 game:GetService("TweenService"):Create(game.Lighting, TweenInfo.new(0.01), {FogEnd = 0}):Play()
 game:GetService("TweenService"):Create(game.Lighting, TweenInfo.new(0.01), {FogStart = 0}):Play()
