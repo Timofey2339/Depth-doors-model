fb41847f1671f8b832d8016b318e250ea4a53ee1
Timofey2339 <kirpatimofey@gmail.com>
2026-07-07 05:00:19 +0300

    Refactor sound asset loading for dynamic handling
    Updated sound handling to dynamically find and set sound properties for both Yellow and Blue music assets.

--- Mixed Mode Settings/Custom Sounds.lua
+++ Mixed Mode Settings/Custom Sounds.lua
@@ -5,15 +5,24 @@ local function getGitSoundId(GithubSoundPath: string, AssetName: string): Sound
         writefile(AssetName..".mp3", game:HttpGet(Url)) 
     end
 
-    local Sound = game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game.Health.Music.Yellow
-    Sound.SoundId = getcustomasset(AssetName..".mp3", true)
+    local Sound = game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game.Health.Music:FindFirstChild(AssetName)
+    if Sound then
+        Sound.SoundId = getcustomasset(AssetName..".mp3", true)
+    end
     return Sound 
 end
 
-local CustomMusic = getGitSoundId("https://github.com/Timofey2339/Depth-doors-model/raw/refs/heads/main/CustomDedMusic.mp3", "Yellow")
-CustomMusic.Parent = game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game.Health.Music
-CustomMusic.Looped = true
-CustomMusic.Volume = 2
+local CustomMusicYellow = getGitSoundId("https://github.com/Timofey2339/Depth-doors-model/raw/refs/heads/main/CustomDedMusic.mp3", "Yellow")
+if CustomMusicYellow then
+    CustomMusicYellow.Looped = true
+    CustomMusicYellow.Volume = 2
+end
+
+local CustomMusicBlue = getGitSoundId("https://github.com/Timofey2339/Depth-doors-model/raw/refs/heads/main/CustomDedMusic.mp3", "Blue")
+if CustomMusicBlue then
+    CustomMusicBlue.Looped = true
+    CustomMusicBlue.Volume = 2
+end
 	
 coroutine.wrap(function()
 	while true do
