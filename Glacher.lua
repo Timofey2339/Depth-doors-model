3056e66726cc9f492ea1cd3e14e8b4911cded38a
Timofey2339 <kirpatimofey@gmail.com>
2026-07-07 13:14:39 +0300

    Update Glacher asset ID in Glacher.lua

--- Glacher.lua
+++ Glacher.lua
@@ -14,7 +14,7 @@ local spawner = loadstring(game:HttpGet("https://raw.githubusercontent.com/Regul
 local entity = spawner.Create({
         Entity = {
             Name = "Glacher",
-            Asset = "rbxassetid://134768877241052",
+            Asset = "rbxassetid://75933370832645",
             HeightOffset = 0
         },
         Lights = {
