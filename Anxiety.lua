7eb9f0c3d933b56bbe6796a125ad0aa188c7c5c8
Timofey2339 <kirpatimofey@gmail.com>
2026-08-12 11:04:46 +0300

    Implement Anxiety entity with jumpscare mechanics

---
+++ Anxiety.lua
@@ -0,0 +1,166 @@
+coroutine.wrap(function()
+    while true do
+        task.wait(0.1)
+        game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+        
+        if workspace:FindFirstChild("SeekMovingNewClone") or workspace.CurrentRooms:FindFirstChild("50") then
+            game.Workspace:FindFirstChild("Anxiety"):Destroy()
+			return
+        end
+    end
+end)()
+local spawner = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Entity%20Spawner/V2/Source.lua"))()	
+local entity = spawner.Create({
+        Entity = {
+            Name = "Anxiety",
+            Asset = "rbxassetid://121666697064953",
+            HeightOffset = 0
+        },
+        Lights = {
+            Flicker = { Enabled = true, Duration = 4},
+            Shatter = true, Repair = true
+        },
+        Earthquake = { Enabled = true },
+        CameraShake = { Enabled = true, Range = 100, Values = {0.2, 0.3, 0.1, 0.2} },
+                Jumpscare = {
+            false, -- Enabled
+            {
+                Image1 = "rbxassetid://10483855823", -- A-60 Jumpscare Image
+                Image2 = "rbxassetid://11360803115",
+                Shake = true,
+                Sound1 = {18459521002, 1}, -- Jumpscare sound
+                Sound2 = {18459521002, 1},
+                Flashing = {true, Color3.fromRGB(255, 0, 0)},
+                Tease = {true, Min = 1, Max = 3},
+            },
+        },
+        Movement = { Speed = 300, Delay = 6, Reversed = false },
+        Rebounding = { Enabled = true, Type = "Ambush", Min = 1, Max = 3, Delay = 3.0 },
+        Damage = { Enabled = true, Range = 40, Amount = 1 },
+        Crucifixion = { Enabled = true, Range = 40, Resist = false, Break = true },
+        Death = {
+            Type = "Curious",
+            Hints = {"You died by Nightmare Apex", "If the light starts to flicker", "hide","Bye Bye"}, 
+            Cause = "Anxiety"
+        }
+    })
+
+    entity:SetCallback("OnSpawned", function()
+    local Sound = Instance.new("Sound")
+Sound.Parent = game.Workspace
+Sound.SoundId = "rbxassetid://9114224217"
+Sound.Volume = 6
+local distort = Instance.new("DistortionSoundEffect")
+distort.Parent = Sound
+distort.Level = 0.75
+Sound:Play()
+wait(5)
+        SetAtmosphere(Color3.fromRGB(85, 150, 255), 0.5)
+        local model = game.Workspace:FindFirstChild("Anxiety")
+        if model then
+            for _, v in pairs(model:GetDescendants()) do
+                if v:IsA("Sound") then
+                    v.SoundId = "rbxassetid://111930358743197"
+                    v.Volume = 2
+                    v.PlaybackSpeed = 1
+                elseif v:IsA("BasePart") then
+                    v.Color = Color3.fromRGB(0, 0, 80)
+                    v.Material = Enum.Material.Neon
+                    local light = Instance.new("PointLight", v)
+                    light.Color = Color3.fromRGB(0, 255, 255)
+                    light.Range = 60; light.Brightness = 6; light.Shadows = true
+                elseif v:IsA("Decal") then
+                    v.Texture = "rbxassetid://11118765532"
+                end
+            end
+        end
+    end)
+
+entity:SetCallback("OnDamagePlayer", function(newHealth)
+		-- UI Construction
+    local JumpscareGui = Instance.new("ScreenGui")
+    local Background = Instance.new("Frame")
+    local Face = Instance.new("ImageLabel")
+	local SizeValue = Instance.new("NumberValue")
+
+	SizeValue.Name = "SizeValue"
+	SizeValue.Value = -0.025
+
+    JumpscareGui.Name = "Jumpscare"
+    JumpscareGui.IgnoreGuiInset = true
+    JumpscareGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+    JumpscareGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+
+    Background.Name = "Frame"
+    Background.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+    Background.BorderSizePixel = 0
+    Background.Size = UDim2.new(1, 0, 1, 0)
+    Background.ZIndex = 50
+
+    Face.Name = "ImageLabel"
+    Face.AnchorPoint = Vector2.new(0.5, 0.5)
+    Face.BackgroundTransparency = 1
+    Face.Position = UDim2.new(0.5, 0, 0.5, 0)
+    Face.ResampleMode = Enum.ResamplerMode.Pixelated
+    Face.Size = UDim2.new(0.35, 0, 0.4, 0)
+    Face.Image = "rbxassetid://11453657080"
+
+local scare = Instance.new("Sound")
+scare.Parent = Background
+scare.Name = "Jumpscare"
+scare.SoundId = "rbxassetid://11984351"
+scare.PlaybackSpeed = 1.6
+scare.Volume = 10
+
+local shift = Instance.new("ReverbSoundEffect")
+shift.DecayTime = 1.5
+shift.Density = 1
+shift.Diffusion = 1
+shift.DryLevel = -6
+shift.Priority =0
+shift.WetLevel = 0
+shift.Parent = scare
+
+local distort = Instance.new("DistortionSoundEffect")
+distort.Parent = scare
+distort.Level = 0.75
+
+    Background.Parent = JumpscareGui
+    Face.Parent = Background
+	SizeValue.Parent = Background
+wait(0.1)
+local v2 = Random.new()
+SizeValue.Value = -0.025
+scare:Play()
+game.ReplicatedStorage.GameStats["Player_" .. game.Players.LocalPlayer.Name]["Total"].DeathCause.Value = "Anxiety"
+local reasons = {"You died by Anxiety", "If lights starts blinking hide"}
+local v4 = math.random(1, 2)
+for v5 = 1, 70 do
+	local v6 = math.random(1, 3)
+	if v6 == 1 then
+		Background.BackgroundColor3 = Color3.new(1, 0.666667, 0)
+		Face.ImageColor3 = Color3.new(1, 1, 1)
+	end
+	if v6 == 2 then
+		Background.BackgroundColor3 = Color3.new(1, 1, 1)
+		Face.ImageColor3 = Color3.new(0, 0, 0)
+	end
+	if v6 == 3 then
+		Background.BackgroundColor3 = Color3.new(0.454902, 0.301961, 0)
+		Face.ImageColor3 = Color3.new(0.490196, 0.490196, 0.490196)
+	end
+	Face.Position = UDim2.new(v2:NextNumber(0.4, 0.6), 0,v2:NextNumber(0.4, 0.6), 0)
+	Face.Size = Face.Size + UDim2.new(SizeValue.Value / 999, 0, 0.001, 0)
+	Face.Rotation = math.random(-20, 20)
+	SizeValue.Value = SizeValue.Value + 0.3
+	wait(0)
+		end
+game.ReplicatedStorage.GameStats["Player_" .. game.Players.LocalPlayer.Name]["Total"].DeathReason.Value = reasons[1]
+Face.ImageColor3 = Color3.new(1, 1, 1)
+Background.BackgroundColor3 = Color3.new(0, 0, 0)
+game.Players.LocalPlayer.Character.Humanoid.Health = 0
+JumpscareGui.Enabled = false
+end)
+
+    entity:SetCallback("OnDespawned", function() ClearAtmosphere() end)
+    entity:Run()
