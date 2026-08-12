7d564924c477dc3cf959d948514e2d575919f538
Timofey2339 <kirpatimofey@gmail.com>
2026-08-12 11:09:14 +0300

    Update version text to include new entities

--- Update log.lua
+++ Update log.lua
@@ -31,7 +31,7 @@ TextUpdate.TextSize = 8
 TextUpdate.RichText = true
 TextUpdate.TextWrapped = true
 TextUpdate.TextScaled = true
-TextUpdate.Text = "Version 1.9, new entitiy added! (Nightmare Apex) and Rambey's Jumpscare Updated"
+TextUpdate.Text = "Version 1.9, 2 new entities added! (Nightmare Apex), and (Anxiety) and Rambey's Jumpscare Updated"
 
 local buttonClose = Instance.new("TextButton") 
 buttonClose.BackgroundTransparency = 0.5
