25ccc16e0a4f8382a2a018daaa215b83798c0cb1
Timofey2339 <kirpatimofey@gmail.com>
2026-08-13 14:22:23 +0300

    Implement SeekOld.lua for SeekMovingNewClone handling
    Add coroutine to manage SeekMovingNewClone visibility and effects.

---
+++ Mixed Mode Settings/SeekOld.lua
@@ -0,0 +1,25 @@
+coroutine.wrap(function()
+    while true do
+        wait(5)
+        game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+        
+        local seekClone = workspace:FindFirstChild("SeekMovingNewClone")
+      
+        if seekClone and seekClone:FindFirstChild("SeekRig") then
+            local seekRig = seekClone.SeekRig
+      seekClone.Figure.Transparency = 0
+      seekClone.Figure.Scream.FlangeSoundEffect.Enabled = false
+      seekClone.Figure.Attachment.ParticleEmitter.Enabled = true
+      seekClone.Figure.CamAttach.SpotLight.Enabled = true
+
+           
+            for _, object in ipairs(seekRig:GetDescendants()) do
+                if object:IsA("BasePart") then
+                    object.Transparency = 1
+                elseif object:IsA("Decal") or object:IsA("Texture") then
+                    object.Transparency = 1
+                end
+            end
+        end
+    end
+end)()
