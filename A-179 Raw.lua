8e6c496fee9d39f92e0a1e075e96e8790f689f23
Timofey2339 <kirpatimofey@gmail.com>
2026-08-24 19:46:15 +0300

    Update A-179 Raw.lua

--- A-179 Raw.lua
+++ A-179 Raw.lua
@@ -4,7 +4,7 @@ coroutine.wrap(function()
         game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
         
         if workspace:FindFirstChild("SeekMovingNewClone") or workspace.CurrentRooms:FindFirstChild("50") then
-            game.Workspace:WaitForChild("A-179", 5):Destroy()
+            game.Workspace:FindFirstChild("A-179", 5):Destroy()
 			return
         end
     end
