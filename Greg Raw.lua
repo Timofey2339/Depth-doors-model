c5ec4867118424ad06a5116d147ee7b78191b82f
Timofey2339 <kirpatimofey@gmail.com>
2026-08-24 17:57:12 +0300

    Update Greg Raw.lua

--- Greg Raw.lua
+++ Greg Raw.lua
@@ -4,7 +4,7 @@ coroutine.wrap(function()
         game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
         
         if workspace:FindFirstChild("SeekMovingNewClone") or workspace.CurrentRooms:FindFirstChild("50") then
-            game.Workspace:FindFirstChild("Greg"):Destroy()
+            game.Workspace:FindFirstChild("Greg", 5):Destroy()
 			return
         end
     end
