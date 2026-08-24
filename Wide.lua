d7c3995639c88fa6b51639ccdb2ff130da89d8e4
Timofey2339 <kirpatimofey@gmail.com>
2026-08-24 23:15:02 +0300

    Implement jumpscare on player health depletion
    Add jumpscare functionality when player's health reaches zero.

--- Wide.lua
+++ Wide.lua
@@ -70,7 +70,86 @@ wait(5)
     end)
 
 entity:SetCallback("OnDamagePlayer", function(newHealth)
-    
+    if newhealth = 0 then
+		    local JumpscareGui = Instance.new("ScreenGui")
+    local Background = Instance.new("Frame")
+    local Face = Instance.new("ImageLabel")
+	local SizeValue = Instance.new("NumberValue")
+
+	SizeValue.Name = "SizeValue"
+	SizeValue.Value = -0.025
+
+    JumpscareGui.Name = "Jumpscare"
+    JumpscareGui.IgnoreGuiInset = true
+    JumpscareGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+    JumpscareGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+
+    Background.Name = "Frame"
+    Background.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+    Background.BorderSizePixel = 0
+    Background.Size = UDim2.new(1, 0, 1, 0)
+    Background.ZIndex = 50
+
+    Face.Name = "ImageLabel"
+    Face.AnchorPoint = Vector2.new(0.5, 0.5)
+    Face.BackgroundTransparency = 1
+    Face.Position = UDim2.new(0.5, 0, 0.5, 0)
+    Face.ResampleMode = Enum.ResamplerMode.Pixelated
+    Face.Size = UDim2.new(0.35, 0, 0.4, 0)
+    Face.Image = "http://www.roblox.com/asset/?id=11226296371"
+
+local scare = Instance.new("Sound")
+scare.Parent = Background
+scare.Name = "Jumpscare"
+scare.SoundId = "rbxassetid://113366757765870"
+scare.PlaybackSpeed = 1.3
+scare.Volume = 5
+
+local shift = Instance.new("ChorusSoundEffect")
+shift.Depth = 1
+shift.Mix = 0.75
+shift.Priority = 0
+shift.Rate = 0.4
+shift.Parent = scare
+
+local distort = Instance.new("DistortionSoundEffect")
+distort.Parent = scare
+distort.Level = 1
+
+    Background.Parent = JumpscareGui
+    Face.Parent = Background
+	SizeValue.Parent = Background
+wait(0.1)
+local v2 = Random.new()
+SizeValue.Value = 0
+local ModelEntity = game.Workspace:FindFirstChild("Wide")
+if ModelEntity then
+    ModelEntity.RushNew.Footsteps.Volume = 0
+    ModelEntity.RushNew.PlaySound.Volume = 0
+end
+scare:Play()
+local v4 = math.random(1, 2)
+for v5 = 1, 35 do
+	local v6 = math.random(1, 3)
+	if v6 == 1 then
+		Background.BackgroundColor3 = Color3.new(0, 0, 0)
+		Face.ImageColor3 = Color3.new(1, 1, 1)
+	end
+	if v6 == 2 then
+		Background.BackgroundColor3 = Color3.new(1, 0, 1)
+		Face.ImageColor3 = Color3.new(0, 0, 0)
+	end
+	if v6 == 3 then
+		Background.BackgroundColor3 = Color3.new(0.52549, 0, 0.52549)
+		Face.ImageColor3 = Color3.new(1, 0, 1)
+	end
+	Face.Position = UDim2.new(v2:NextNumber(0.4, 0.6), 0, v2:NextNumber(0.45, 0.55), 0)
+	Face.Size = Face.Size + UDim2.new(SizeValue.Value, 0, SizeValue.Value, 0)
+	Face.Rotation = math.random(-10, 10)
+	SizeValue.Value = SizeValue.Value + 0.004
+	wait(0)
+		end
+	end
 end)
 
     entity:SetCallback("OnDespawned", function() ClearAtmosphere() end)
