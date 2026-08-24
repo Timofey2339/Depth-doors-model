583e8cd7fc92bd7f778d391d19f7069a1b1b4fc1
Timofey2339 <kirpatimofey@gmail.com>
2026-08-24 23:03:32 +0300

    Update Spark.lua

--- Spark.lua
+++ Spark.lua
@@ -116,7 +116,7 @@ scare.Parent = Background
 scare.Name = "Jumpscare"
 scare.SoundId = "rbxassetid://5567155845"
 scare.PlaybackSpeed = 3
-scare.Volume = 7
+scare.Volume = 5
 
 local shift = Instance.new("PitchShiftSoundEffect")
 shift.Octave = 0.5
@@ -131,6 +131,11 @@ distort.Level = 0.98
 	SizeValue.Parent = Background
 
 SizeValue.Value = -0.05
+local ModelEntity = game.Workspace:FindFirstChild("Spark")
+if ModelEntity then
+    ModelEntity.RushNew.Footsteps.Volume = 0
+    ModelEntity.RushNew.PlaySound.Volume = 0
+end
 scare:Play()
 local u2 = Random.new()
 local function v3()
