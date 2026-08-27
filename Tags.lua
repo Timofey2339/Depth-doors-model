03703661180912bb24c64e03530d4c1fe2732509
Timofey2339 <kirpatimofey@gmail.com>
2026-08-28 01:08:57 +0300

    Update Tags.lua

--- Tags.lua
+++ Tags.lua
@@ -12,7 +12,7 @@ end
 local function spawnApex()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Timofey2339/Depth-doors-model/refs/heads/main/Nightmare%20Apex"))()
 end
-local function spawnMoxoxide()
+local function spawnMonoxide() -- Виправлено назву з spawnMoxoxide на spawnMonoxide
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Timofey2339/Depth-doors-model/refs/heads/main/Monoxide.lua"))()
 end
 local function spawnRambey()
@@ -62,34 +62,40 @@ TextChatService.OnIncomingMessage = function(message)
             properties.PrefixText = "<font color='#FF0000'>[OWNER Of the mode]</font> " .. message.PrefixText
             
             local text = string.lower(message.Text)
-            if text == "/Greg" then
+            
+            -- Усі команди переведено в нижній регістр
+            if text == "/greg" then
                 spawnGreg()
-            elseif text == "/Drave" then
+            elseif text == "/drave" then
                 spawnDrave()
-            elseif text == "/Rebound" then
+            elseif text == "/rebound" then
                 spawnRebound()
-            elseif text == "/Spark" then
+            elseif text == "/spark" then
                 spawnSpark()
-            elseif text == "/NightmareApex" then
+            elseif text == "/nightmareapex" then
                 spawnApex()
-            elseif text == "/Monoxide" then
+            elseif text == "/monoxide" then
                 spawnMonoxide()
-            elseif text == "/Dread" then
+            elseif text == "/dread" then
                 spawnDread()
-            elseif text == "/Ankle" then
+            elseif text == "/ankle" then
                 spawnAnkle()
-           elseif text == "/Watcher" then
+            elseif text == "/watcher" then
                 spawnWatcher()
-            elseif text == "/A179" then
+            elseif text == "/a179" then
                 spawnA179()
-            elseif text == "/A60" then
+            elseif text == "/a60" then
                 spawnA60()
-            elseif text == "/Anxiety" then
+            elseif text == "/anxiety" then
                 spawnAnxiety()
-            elseif text == "/Anxiety" then
-                spawnAnxiety()
-            elseif text == "/A35Admin" then
+            elseif text == "/a35admin" then
                 spawnA35()
+            elseif text == "/rambey" then
+                spawnRambey()
+            elseif text == "/glacher" then
+                spawnGlacher()
+            elseif text == "/wide" then
+                spawnWide()
             end
         end
     end
