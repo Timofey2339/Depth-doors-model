61aa772300d7e0c1761980d772799f50d12952a9
Timofey2339 <kirpatimofey@gmail.com>
2026-09-08 20:54:54 +0300

    Update Fanmade Dread.lua

--- Fanmade Dread.lua
+++ Fanmade Dread.lua
@@ -16,17 +16,31 @@ local pitch = Instance.new("ReverbSoundEffect")
 pitch.Parent = sound2
 
 sound:Play()
-coroutine.wrap(function()
-    while true do
-        task.wait(0.1)
-        game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
-        
-        if workspace:FindFirstChild("SeekMovingNewClone") or workspace.CurrentRooms:FindFirstChild("50") then
-            game.Workspace:FindFirstChild("Fanmade Dread", 5):Destroy()
-            return
-        end
+local workspace = game:GetService("Workspace")
+local replicatedStorage = game:GetService("ReplicatedStorage")
+local currentRooms = workspace:WaitForChild("CurrentRooms")
+
+if workspace:FindFirstChild("SeekMovingNewClone") or currentRooms:FindFirstChild("50") then
+    return
+end
+
+local shouldCancel = false
+
+local seekConn = workspace.ChildAdded:Connect(function(child)
+    if child.Name == "SeekMovingNewClone" then
+        shouldCancel = true
+        local model = workspace:FindFirstChild("Fanmade Dread")
+        if model then model:Destroy() end
     end
-end)()
+end)
+
+local roomConn = currentRooms.ChildAdded:Connect(function(child)
+    if child.Name == "50" then
+        shouldCancel = true
+        local model = workspace:FindFirstChild("Fanmade Dread")
+        if model then model:Destroy() end
+    end
+end)
 sound2:Play()
 
 local Warneffect = Instance.new("ColorCorrectionEffect",game.Lighting)
