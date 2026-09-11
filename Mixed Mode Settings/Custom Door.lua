e513716c57bf465a6a1d0831dca6c7f041bff8a1
Timofey2339 <kirpatimofey@gmail.com>
2026-09-11 11:57:14 +0300

    Prevent door changes for room '50'
    Refactor door handling logic to prevent changes in room '50'.

--- Mixed Mode Settings/Custom Door.lua
+++ Mixed Mode Settings/Custom Door.lua
@@ -1,37 +1,35 @@
 local rooms = workspace.CurrentRooms
 
 local function changeDoor(room)
+	if room.Name == "50" then
+		return
+	end
+
 	local door = room:FindFirstChild("Door")
 	if door then
-		
 		local NewDoor = game:GetObjects("rbxassetid://81616085996734")[1]
 		NewDoor.Parent = room
 		
-		-- робимо стару дверь невидимою
 		for _,v in pairs(door:GetDescendants()) do
 			if v:IsA("BasePart") then
 				v.Transparency = 1
 			end
 		end
 		
-		-- слідкуємо за позицією
 		task.spawn(function()
-			while door.Parent do
+			while door and door.Parent do
 				NewDoor:PivotTo(door:GetPivot())
 				task.wait()
 			end
 		end)
-		
 	end
 end
 
--- для нових кімнат
 rooms.ChildAdded:Connect(function(room)
 	task.wait(0.5)
 	changeDoor(room)
 end)
 
--- для існуючих
 for _,room in pairs(rooms:GetChildren()) do
 	changeDoor(room)
 end
